9cccd29e11f4f8d55414c0cf455f47ba6cbaa772
codeskyblue <codeskyblue@gmail.com>
2015-12-07 19:35:48 +0800

    start to modify mem

--- mem/mem_linux.go
+++ mem/mem_linux.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 	"syscall"
 
-	"github.com/shirou/gopsutil/internal/common"
+	"github.com/codeskyblue/gopsutil/internal/common"
 )
 
 func VirtualMemory() (*VirtualMemoryStat, error) {
